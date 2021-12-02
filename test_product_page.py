24bd3c2cf88a16b6b9a96eeeccfa9e42096b202d
anaidych <anaidych@softjourn.com>
2021-12-02 21:37:41 +0200

    Update test_product_page.py test.

--- test_product_page.py
+++ test_product_page.py
@@ -50,6 +50,30 @@ def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     basket_page.should_not_be_item_in_basket()
     basket_page.should_be_basket_is_empty_message()
 
+@pytest.mark.xfail
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    product_page = ProductPage(browser, link)
+    product_page.open()
+    product_page.add_item_to_basket()
+    product_page.should_not_be_success_message()
+
+
+def test_guest_cant_see_success_message(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    product_page = ProductPage(browser, link)
+    product_page.open()
+    product_page.should_not_be_success_message()
+
+
+@pytest.mark.xfail
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    product_page = ProductPage(browser, link)
+    product_page.open()
+    product_page.add_item_to_basket()
+    product_page.should_wait_until_element_disappears()
+
 
 @pytest.mark.register_user
 class TestUserAddToBasketFromProductPage():
